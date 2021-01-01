dffc5a8bf2e7e16eae0379a02b2a382c2d4d9139
Donjeta <donjeta.vojvoda@epitech.eu>
2021-01-01 18:18:59 +0100

    user register

---
+++ dashboard/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./model/user')
+
+mongoose.connect('mongodb://localhost:27017/login-app-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const app = express()
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json())
+
+app.post('/api/register', async (req, res) => {
+    console.log(req.body)
+    res.json({ status: 'ok' })
+})
+
+app.listen(9090, function () {
+    console.log('Server up on port 9090!');
+});
